8f9e92dbd2d73b48366c798606b402e037c51cb0
boll <48569460+clubmate@users.noreply.github.com>
2020-05-16 19:01:52 +0200

    RC-0

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
 // AUS CURRENTPHOTO UND CURRENTOHOTO+1 IRGENDWIE DOCH LIEBER LINKS UND RECHTS MACHEN
-// DIE BILDER BRAUCHEN NOCH NE ID UND "GEWONNEN GEGEN" UND "VERLOREN GEGEN"
+// DETAILANSICHT PRO BILD GEGEN WELCHE ES GEWONNEN ODER VERLOREN HAT
+// PRO DURCHLAUF MUSS ES EINE ZAHL GEBEN WIE NAH MAN SICH AN DIE ELO-PREDICTION ANGENAEHERT HAT 
 
 var jsonfile = '{ "photos" : [' +
-'{ "uri":"test-1.jpg" , "score":1000 , "wins":0 , "losses":0},' +
-'{ "uri":"test-2.jpg" , "score":1000 , "wins":0 , "losses":0},' +
-'{ "uri":"test-3.jpg" , "score":1000 , "wins":0 , "losses":0},' +
-'{ "uri":"test-4.jpg" , "score":1000 , "wins":0 , "losses":0},' +
-'{ "uri":"test-5.jpg" , "score":1000 , "wins":0 , "losses":0},' +
-'{ "uri":"test-6.jpg" , "score":1000 , "wins":0 , "losses":0} ]}';
+'{ "id":1 , "uri":"test-7.jpg" , "score":1000 , "wins":[] , "losses":[] },' +
+'{ "id":2 , "uri":"test-7.jpg" , "score":1000 , "wins":[] , "losses":[] },' +
+'{ "id":3 , "uri":"test-7.jpg" , "score":1000 , "wins":[] , "losses":[] },' +
+'{ "id":4 , "uri":"test-8.jpg" , "score":1000 , "wins":[] , "losses":[] },' +
+'{ "id":5 , "uri":"test-8.jpg" , "score":1000 , "wins":[] , "losses":[] },' +
+'{ "id":20 , "uri":"test-8.jpg" , "score":1000 , "wins":[] , "losses":[] } ]}';
 
 var json = JSON.parse(jsonfile);
 var currentPhoto = -2;
 var points = $.map(json.photos, function(value, index){
         return [value];
     });
+var correctAnswer = 0;
+var round = 1;
 
 // The maximum number of points a player goes up or down by
 // A 50% chance of winning (aka, both players have the same rating) means they go up or down by 32/2 = 16 points.
@@ -52,12 +55,43 @@ function makeTable() {
   });
 
   $("#completeTable table").empty();
+  $("#completeTable table").append("<tr class=line><th>#</th><th>NAME</th><th>W</th><th>L</th><th>ELO</th></tr>")
   for (var i = 0; i < points.length; i++) {
-    $("#completeTable table").append("<tr><td>" + i + ".</td><td>" + points[i].uri + "</td><td>" + points[i].score.toFixed(0) + "</td><td>" + points[i].wins + "</td><td>" + points[i].losses + "</td></tr>")
+  
+    if ((i == Math.floor(points.length*0.25-1)) || (i == Math.floor(points.length*0.50-1)) || (i == Math.floor(points.length*0.75-1))) {
+ 
+      if ((json.photos[currentPhoto].id == points[i].id) || (json.photos[currentPhoto+1].id == points[i].id)) {
+        $("#completeTable table").append("<tr class=\"highlight line\"><td>" + (i+1) + ".</td><td>" + points[i].uri + "</td><td>" + points[i].wins.length + "</td><td>" + points[i].losses.length + "</td><td>" + points[i].score.toFixed(0) + "</td></tr>")
+      } else {
+        $("#completeTable table").append("<tr class=line><td>" + (i+1) + ".</td><td>" + points[i].uri + "</td><td>" + points[i].wins.length + "</td><td>" + points[i].losses.length + "</td><td>" + points[i].score.toFixed(0) + "</td></tr>")
+      } 
+    } else {
+      
+      if ((json.photos[currentPhoto].id == points[i].id) || (json.photos[currentPhoto+1].id == points[i].id)) {
+        $("#completeTable table").append("<tr class=highlight><td>" + (i+1) + ".</td><td>" + points[i].uri + "</td><td>" + points[i].wins.length + "</td><td>" + points[i].losses.length + "</td><td>" + points[i].score.toFixed(0) + "</td></tr>")
+      } else {
+        $("#completeTable table").append("<tr><td>" + (i+1) + ".</td><td>" + points[i].uri + "</td><td>" + points[i].wins.length + "</td><td>" + points[i].losses.length + "</td><td>" + points[i].score.toFixed(0) + "</td></tr>")
+      }
+      
+    }  
+    
   }
 
 }
 
+function makeRoundStats() {
+
+  percent = correctAnswer*100/(currentPhoto/2);
+
+  $("#round").html("<table><tr><td>ROUND " + round + "</td><td>" + (currentPhoto/2) + "/" + (json.photos.length/2) + "</td></tr></table>");
+  
+  if (isNaN(percent)) {
+    $("#roundAccuracy").text("0%");
+  } else {
+    $("#roundAccuracy").text(percent.toFixed(0) + "%");
+  }
+
+}
 
 function updatePhotos() {
 
@@ -65,6 +99,10 @@ function updatePhotos() {
   if (currentPhoto == (json.photos.length-2)) {
     shuffle(json.photos);
     currentPhoto = 0;
+    correctAnswer = 0;
+    $("#roundStats table").append("<tr><td>ROUND " + round + "</td><td>" + percent.toFixed(0) + "%</td></tr>");
+    round++;
+    console.log(percent.toFixed(0) + "%")
     console.log("NEW SHUFFLE")
   } else { currentPhoto += 2; }
  
@@ -80,57 +118,113 @@ function updatePhotos() {
   // Output
   $("#leftPhoto").attr("src","photos/" + json.photos[currentPhoto].uri);
   $("#rightPhoto").attr("src","photos/" + json.photos[currentPhoto+1].uri);
-  
-  $("#left .uri").text(json.photos[currentPhoto].uri);
-  $("#left .wins").text("WINS: " + json.photos[currentPhoto].wins);
-  $("#left .losses").text("LOSSES: " + json.photos[currentPhoto].losses);
-  $("#left .score").text("SCORE: " + json.photos[currentPhoto].score);
-  $("#left .chance").text("CHANCE: " + chances.left.toFixed(3));
+   
+  $("#previewLeftPhoto").attr("src","photos/" + json.photos[currentPhoto].uri);
+  $("#previewRightPhoto").attr("src","photos/" + json.photos[currentPhoto+1].uri);
+ 
+  $("#leftStats .name").text(json.photos[currentPhoto].uri + " (" + json.photos[currentPhoto].wins.length + "-" + json.photos[currentPhoto].losses.length + ")");
+  $("#leftStats .chance").text((chances.left*100).toFixed(0) + "%");
+  $("#leftStats .score").text(json.photos[currentPhoto].score.toFixed(0));
 
-  $("#right .uri").text(json.photos[currentPhoto+1].uri);
-  $("#right .wins").text("WINS: " + json.photos[currentPhoto+1].wins);
-  $("#right .losses").text("LOSSES: " + json.photos[currentPhoto+1].losses);
-  $("#right .score").text("SCORE: " + json.photos[currentPhoto+1].score);
-  $("#right .chance").text("CHANCE: " + chances.right.toFixed(3));
+  $("#rightStats .name").text(json.photos[currentPhoto+1].uri + " (" + json.photos[currentPhoto+1].wins.length + "-" + json.photos[currentPhoto+1].losses.length + ")");
+  $("#rightStats .chance").text((chances.right*100).toFixed(0) + "%");
+  $("#rightStats .score").text(json.photos[currentPhoto+1].score.toFixed(0));
+
 
   makeTable();
+  
+  makeRoundStats();
 
 }
 
 
+function saveJson(text, filename){
+  var a = document.createElement('a');
+  a.setAttribute('href', 'data:text/plain;charset=utf-8,'+encodeURIComponent(text));
+  a.setAttribute('download', filename);
+  a.click()
+}
 
-$("#leftPhoto").click(function() {
+$("#download").click(function() {
+  saveJson(JSON.stringify(json), "filename.json");
+});
+
+
+function leftPhotoWins() {
 
   // DEBUGS
-  console.log(json.photos[currentPhoto].uri + " (S:" + json.photos[currentPhoto].score.toFixed(0) + "|C:" + chances.left.toFixed(2) + "|W:" + json.photos[currentPhoto].wins + "|L:" + json.photos[currentPhoto].losses + ") wins against " + json.photos[currentPhoto+1].uri + " (S:" + json.photos[currentPhoto+1].score.toFixed(0) + "|C:" + chances.right.toFixed(2) + "|W:" + json.photos[currentPhoto+1].wins + "|L:" + json.photos[currentPhoto+1].losses + ").");
+  console.log(json.photos[currentPhoto].uri + " (S:" + json.photos[currentPhoto].score.toFixed(0) + "|C:" + chances.left.toFixed(2) + "|W:" + json.photos[currentPhoto].wins.length + "|L:" + json.photos[currentPhoto].losses.length + ") wins against " + json.photos[currentPhoto+1].uri + " (S:" + json.photos[currentPhoto+1].score.toFixed(0) + "|C:" + chances.right.toFixed(2) + "|W:" + json.photos[currentPhoto+1].wins.length + "|L:" + json.photos[currentPhoto+1].losses.length + ").");
 
   // LEFT WINS
-  json.photos[currentPhoto].wins++;
-  json.photos[currentPhoto+1].losses++;
-  
+  json.photos[currentPhoto].wins.push(json.photos[currentPhoto+1].id);
+  json.photos[currentPhoto+1].losses.push(json.photos[currentPhoto].id);
+ 
   // If left photo wins:
   json.photos[currentPhoto].score = json.photos[currentPhoto].score + KFactor * (1 - chances.left)
   json.photos[currentPhoto+1].score = json.photos[currentPhoto+1].score + KFactor * (0 - chances.right) 
 
+  if (chances.left > chances.right) {
+    correctAnswer++
+  }  
+
   updatePhotos();
+}
 
-});
-
-$("#rightPhoto").click(function() {
+function rightPhotoWins() {
 
   // DEBUGS
-  console.log(json.photos[currentPhoto+1].uri + " (S:" + json.photos[currentPhoto+1].score.toFixed(0) + "|C:" + chances.right.toFixed(2) + "|W:" + json.photos[currentPhoto+1].wins + "|L:" + json.photos[currentPhoto+1].losses + ") wins against " + json.photos[currentPhoto].uri + " (S:" + json.photos[currentPhoto].score.toFixed(0) + "|C:" + chances.left.toFixed(2) + "|W:" + json.photos[currentPhoto].wins + "|L:" + json.photos[currentPhoto].losses + ").");
+  console.log(json.photos[currentPhoto+1].uri + " (S:" + json.photos[currentPhoto+1].score.toFixed(0) + "|C:" + chances.right.toFixed(2) + "|W:" + json.photos[currentPhoto+1].wins.length + "|L:" + json.photos[currentPhoto+1].losses.length + ") wins against " + json.photos[currentPhoto].uri + " (S:" + json.photos[currentPhoto].score.toFixed(0) + "|C:" + chances.left.toFixed(2) + "|W:" + json.photos[currentPhoto].wins.length + "|L:" + json.photos[currentPhoto].losses.length + ").");
 
   // RIGHT WINS
-  json.photos[currentPhoto+1].wins++;
-  json.photos[currentPhoto].losses++;
+  json.photos[currentPhoto+1].wins.push(json.photos[currentPhoto].id);
+  json.photos[currentPhoto].losses.push(json.photos[currentPhoto+1].id);
   
   // If right photowins:
   json.photos[currentPhoto+1].score = json.photos[currentPhoto+1].score + KFactor * (1 - chances.right)
   json.photos[currentPhoto].score = json.photos[currentPhoto].score + KFactor * (0 - chances.left)
     
+  if (chances.right > chances.left) {
+    correctAnswer++
+  }  
+    
   updatePhotos();
+
+}
+
+
+$("#leftPhoto").click(function() {
+
+  leftPhotoWins();
+
+});
+
+$("#rightPhoto").click(function() {
+
+  rightPhotoWins();
   
+});
+
+// KEYBOARD SHORTCUTS
+$('html').keydown(function(e) {
+
+  if (e.keyCode == 37) { leftPhotoWins(); } // LEFT
+  if (e.keyCode == 39) { rightPhotoWins(); } // RIGHT
+
+});
+
+
+// FULLSCREEN
+function activateFullscreen(element) {
+  if (element.requestFullscreen) { element.requestFullscreen(); } // W3C spec
+  else if (element.mozRequestFullScreen) { element.mozRequestFullScreen(); } // Firefox
+  else if (element.webkitRequestFullscreen) { element.webkitRequestFullscreen(); } // Safari
+  else if(element.msRequestFullscreen) { element.msRequestFullscreen(); } // IE/Edge
+};
+
+$('#fullscreen').on('click', function() {
+ 
+    activateFullscreen(document.documentElement);
+   
 });
 
 
@@ -138,4 +232,5 @@ $("#rightPhoto").click(function() {
 // INIT
 shuffle(json.photos);
 updatePhotos();
+
 
