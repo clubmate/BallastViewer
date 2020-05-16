e4f5c7244ee7f775a27c6517e014e479f705e444
boll <48569460+clubmate@users.noreply.github.com>
2020-05-16 19:56:49 +0200

    AVEC PYTHON

---
+++ makejson.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+path = "photos/venuskopf"
+photoURIs = os.listdir(path)
+all = []
+
+for num, i in enumerate(photoURIs): 
+ 
+   # python object to be appended 
+   y = {"id":num, 
+         "uri": i, 
+         "score": 1000,
+         "wins":[],
+         "losses":[]
+    } 
+  
+  
+    # appending data 
+   all.append(y) 
+ 
+   
+t = json.dumps(all, indent=4)
+print(t)  
+
+with open('photos.json', 'w') as outfile:
+    json.dump(all, outfile)
